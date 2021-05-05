53fe07392524cbbcb63cea25a50258ba31aa7701
Patrick Lemke <patrick.lemke@espero.tech>
2021-05-05 12:33:30 +0200

    recactor taproot code

--- src/bot.py
+++ src/bot.py
@@ -1,10 +1,8 @@
 import logging
-from telegram.ext import Updater, CommandHandler, JobQueue
+from telegram.ext import Updater, CommandHandler
 from telegram.ext.callbackcontext import CallbackContext
 from telegram.update import Update
-from textwrap import dedent
-from taproot import taproot_signalling_blocks
-import config
+from taproot import taproot_calculate_signalling_statistics
 
 def start_command(update: Update, context: CallbackContext):
     """
@@ -13,88 +11,11 @@ def start_command(update: Update, context: CallbackContext):
     """
     context.bot.send_message(chat_id=update.effective_chat.id, text="Hi, ich bin der Einundzwanzig Community Bot!")
 
-def sort_by_part_of_hashrate(item):
-    """
-    Sorts the mined blocks by the part of the hashrate
-    that the individual miners contributed (ascending)
-    """
-
-    if config.blocks_mined == 0:
-        return 0
-    
-    miner_signal_true = item[1]['signal_true']
-    miner_signal_false = item[1]['signal_false']
-    miner_signal_total = miner_signal_true + miner_signal_false
-
-    part_of_hashrate = miner_signal_total / config.blocks_mined
-
-    return part_of_hashrate
-
 def taproot_command(update: Update, context: CallbackContext):
     """
     Calculates Taproot Activation Statistics
     """
-    try:
-
-        signalling_percentage = 0.0
-
-        # Prevent division by 0
-        if config.signal_true != 0:
-            signalling_percentage = config.signal_true / (config.signal_true + config.signal_false)
-
-        current_cycle_activation_possible = False if config.signal_false > 201 else True
-
-        activation_message = None
-
-        if not current_cycle_activation_possible:
-            activation_message = "Fehlgeschlagen 😭"
-        else: 
-            if config.signal_true >= 1815:
-                activation_message = "Erfolgreich 🎉🎉🎉"
-            else:
-                activation_message = "Möglich 🙏"
-
-        message = dedent(f"""
-        <b>Taproot Aktivierung</b>
-        Geschürfte Blöcke: {config.blocks_mined} / 2016
-        Benötigt: 1815 / 2016 (90%)
-        Signalisieren dafür: {config.signal_true} ({signalling_percentage * 100:.1f}%)
-        Signalisieren nicht: {config.signal_false} ({(1 - signalling_percentage) * 100:.1f}%)
-
-        <b>Aktueller Zyklus</b>
-        Aktivierung: {activation_message}
-
-        <b>Mining Pools</b>        
-        """)
-
-        total_signalling_hashrate = 0.0
-
-        # Sort by part of hash rate
-        miners_sorted = dict(sorted(config.miner_stats.items(), key=lambda item: sort_by_part_of_hashrate(item), reverse=True))
-
-        for miner in miners_sorted.keys():
-
-            miner_signal_true = config.miner_stats[miner]['signal_true']
-            miner_signal_false = config.miner_stats[miner]['signal_false']
-            miner_signal_total = miner_signal_true + miner_signal_false
-
-            # Prevent division by 0 if no blocks have been mined yet in the cycle
-            if config.blocks_mined == 0:
-                part_of_hashrate = 0
-            else:
-                part_of_hashrate = miner_signal_total / config.blocks_mined
-            
-            if miner_signal_true > 0:
-                total_signalling_hashrate += part_of_hashrate
-                message += dedent(f"{miner} ✅ ({miner_signal_true} / {miner_signal_total}) Hash: {part_of_hashrate * 100:.1f}%\n")
-
-        message += dedent(f"\n<b>Summe Hash: {total_signalling_hashrate * 100:.1f}%</b>")
-
-        context.bot.send_message(chat_id=update.effective_chat.id, text=message, parse_mode='HTML', disable_web_page_preview=True)
-
-    except Exception as e:
-        logging.log(logging.ERROR, e)
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Es ist ein Fehler aufgetreten")
+    taproot_calculate_signalling_statistics(update, context)
 
 def run(bot_token: str):
     """
@@ -111,13 +32,5 @@ def run(bot_token: str):
 
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(taproot_handler)
-
-    j = updater.job_queue
-
-    # Fetch statistics every 60 seconds
-    j.run_repeating(taproot_signalling_blocks, interval=60)
-
-    # Run once at startup
-    taproot_signalling_blocks(None)
 
     updater.start_polling()
--- src/config.py
+++ src/config.py
@@ -3,14 +3,5 @@ from typing import List
 # URL of the taproot watch instance
 taproot_watch_url = 'https://taproot.watch'
 
-# List of all signalling miners
-signalling_miners: List[str] = []
-
-blocks_mined = 0
-signal_true = 0
-signal_false = 0
-
-miner_stats = {}
-
 # Telegram Chat id of the Einundzwanzig Chat Group
 einundzwanzig_chat_id = -1001427443561
